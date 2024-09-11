6115f6840b878c530e548ceeffeeb5d432288be0
Gottfried Akuffo <gottfried.akuffo@gmail.com>
2024-09-11 13:35:37 +0000

    frontend completed

--- app/(admin)/orders/_layout.tsx
+++ app/(admin)/orders/_layout.tsx
@@ -1,11 +1,9 @@
-/* eslint-disable prettier/prettier */
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Stack } from 'expo-router';
 
 export default function MenuStack() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Orders' }} />
+      {/* <Stack.Screen name="index" options={{ title: 'Orders' }} /> */}
       <Stack.Screen name="list" options={{ headerShown: false }} />
     </Stack>
   );
--- app/(admin)/orders/list/_layout.tsx
+++ app/(admin)/orders/list/_layout.tsx
@@ -1,12 +1,15 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { Tabs, withLayoutContext } from 'expo-router';
+import { withLayoutContext } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const TopTabs = withLayoutContext(createMaterialTopTabNavigator().Navigator);
 
-export default function OrderlistNavigator() {
+export default function OrderListNavigator() {
   return (
-    <TopTabs>
-      <TopTabs.Screen name="index" options={{ title: 'Active' }} />
-    </TopTabs>
+    <SafeAreaView edges={['top']} style={{ flex: 1, backgroundColor: 'white' }}>
+      <TopTabs>
+        <TopTabs.Screen name="index" options={{ title: 'Active' }} />
+      </TopTabs>
+    </SafeAreaView>
   );
 }
